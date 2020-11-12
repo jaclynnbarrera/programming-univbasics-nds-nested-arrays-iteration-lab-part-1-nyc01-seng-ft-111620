4c964b3c93eb6df4f3cbe54467298be1e72f9f32
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-12 17:03:30 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,14 @@
-#initial thoughts: #set the loop to run through the array, set condition if values return are divisible by 2, print, otherwise increment counter by 1 and keep going
-
 def find_even_values(src)
-  row_index = 0
-  while row_index < src.count do
+  row_index = 0 
+  even_numbers= 0
+  while row_index < src.length do
     element_index = 0
-    while element_index < src[row_index].count do
+    while element_index < src[row_index].length do
       if src[row_index][element_index].even?
         p src[row_index][element_index]
       end
-      element_index += 1
-    end
+        element_index =+ 1 
+      end 
     row_index += 1
-  end
-end
+  end   
+end 
